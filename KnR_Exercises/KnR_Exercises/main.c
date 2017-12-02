3f3f93964688663bdc0676b989bf6d32079c160f
DavitaIgor <kantor_igor@yahoo.com>
2017-12-02 13:31:36 -0700

    updating gitignore

--- KnR_Exercises/KnR_Exercises/main.c
+++ KnR_Exercises/KnR_Exercises/main.c
@@ -6,10 +6,50 @@
 //  Copyright © 2017 Igorware. All rights reserved.
 //
 
+//
+//  main.c
+//  KnR-Exercises
+//
+//  Created by Igor Kantor on 12/2/17.
+//  Copyright © 2017 Igorware. All rights reserved.
+//
+
 #include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+
+// [X] Exercise 1-1. Run the ``hello, world'' program on your system. Experiment with leaving out parts of the program, to see what error messages you get.
+// [X] Exercise 1-2. Experiment to find out what happens when prints's argument string contains \c, where c is some character not listed above.
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // [X] Exercise 2-1. Write a program to determine the ranges of char, short, int, and   //
+    //     long variables, both signed and unsigned, by printing appropriate values from    //
+    //     standard headers and by direct computation. Harder if you compute them: determine//
+    //     the ranges of the various floating-point types.                                  //
+    //////////////////////////////////////////////////////////////////////////////////////////
+//        printf("char %d to %d size %ld\n", CHAR_MIN, CHAR_MAX, sizeof(char));
+//        printf("short %d to %d size %ld\n", SHRT_MIN, SHRT_MAX, sizeof(short));
+//        printf("int %d to %d size %ld\n", INT_MIN, INT_MAX, sizeof(int));
+//        printf("long %ld to %ld size %ld\n", LONG_MIN, LONG_MAX, sizeof(long));
+//    
+//        printf("float %f to %f size %ld\n", FLT_MIN, FLT_MAX, sizeof(float));
+//        printf("double %f to %f size %ld\n", DBL_MIN, DBL_MAX, sizeof(double));
+//    
+//        //    Output
+//        //    char -128 to 127
+//        //    short -128 to 127
+//        //    int -2,147,483,648 to 2,147,483,647
+//        //    long -9223372036854775808 to 9223372036854775807
+    
+    
+    
+    
     return 0;
 }
+
+
+
