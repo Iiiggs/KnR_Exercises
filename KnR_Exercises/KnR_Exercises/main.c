9235eafc40cb7c6a944adfa264feed2d52a120fd
DavitaIgor <kantor_igor@yahoo.com>
2017-12-02 13:28:25 -0700

    initial exercises

---
+++ KnR_Exercises/KnR_Exercises/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  KnR_Exercises
+//
+//  Created by Igor Kantor on 12/2/17.
+//  Copyright © 2017 Igorware. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
